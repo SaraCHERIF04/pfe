6b2e181e51f16be2268594d925a1df775da5e782
farah-boumezebeur <167352086+farah-boumezebeur@users.noreply.github.com>
2025-04-22 19:04:48 +0200

    Update incident_serializer.py

--- backendpfe/accounts/serializers/incident_serializer.py
+++ backendpfe/accounts/serializers/incident_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Incident, Projet, SousProjet, SuivieIncident
+from ..models import Incident, Projet, SousProjet
 
 class IncidentSerializer(serializers.ModelSerializer):
     class Meta:
